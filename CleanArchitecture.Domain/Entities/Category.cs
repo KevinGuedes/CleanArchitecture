ef2130142b50973aea26630a80e9bc6461c563ae
KevinGuedes <kevinguedes@edu.unifor.br>
2021-05-06 19:07:08 -0300

    first commit

---
+++ CleanArchitecture.Domain/Entities/Category.cs
@@ -0,0 +1,46 @@
+﻿using CleanArchitecture.Domain.Validation;
+using System.Collections.Generic;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public sealed class Category : Entity
+    {
+        public string Name { get; private set; }
+
+        public ICollection<Product> Products { get; set; }
+
+        public Category(int id, string name)
+        {
+            ValidateDomain(name);
+            ValidateId(id);
+
+            Name = name;
+            Id = id;
+        }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+
+            Name = name;
+        }
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+
+            Name = name;
+        }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name.");
+            DomainExceptionValidation.When(name.Length < 3, "Name too short. Minimum 3 characters.");
+        }
+
+        private void ValidateId(int id)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid id value.");
+        }
+    }
+}
---
+++ CleanArchitecture.Domain/Entities/Product.cs
@@ -0,0 +1,78 @@
+﻿using CleanArchitecture.Domain.Validation;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public sealed class Product : Entity
+    {
+        public string Name { get; private set; }
+
+        public string Description { get; private set; }
+
+        public decimal Price { get; private set; }
+
+        public int Stock { get; private set; }
+
+        public string Image { get; private set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        public Product(string name, string description, decimal price, int stock, string image)
+        {
+            ValidateDomain(name, description, price, stock, image);
+
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Image = image;
+        }
+
+        public Product(int id, string name, string description, decimal price, int stock, string image)
+        {
+            ValidateDomain(name, description, price, stock, image);
+            ValidateId(id);
+
+            Id = id;
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Image = image;
+        }
+
+        public void Update(string name, string description, decimal price, int stock, string image, int categoryId)
+        {
+            ValidateDomain(name, description, price, stock, image);
+            ValidateId(categoryId);
+
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Image = image;
+            CategoryId = categoryId;
+        }
+
+        private static void ValidateDomain(string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Name is required.");
+            DomainExceptionValidation.When(name.Length < 3, "Name too short. Minimum 3 characters.");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description), "Description is required.");
+            DomainExceptionValidation.When(description.Length < 5, "Description too short. Minimum 4 characters.");
+
+
+            DomainExceptionValidation.When(price < 0, "Invalid price value.");
+            DomainExceptionValidation.When(stock < 0, "Invalid stock value.");
+
+            DomainExceptionValidation.When(image.Length > 250, "Image too long. Maximum 250 characters.");
+        }
+
+        private void ValidateId(int id)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid id value.");
+        }
+    }
+}
